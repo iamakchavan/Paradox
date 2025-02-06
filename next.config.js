3c8d346b0a706254af33e51b981baca3ea09ee6c
AK CHAVAN <akc4d02@gmail.com>
2025-02-06 06:39:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
