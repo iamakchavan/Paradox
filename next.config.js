d255127686a808df5b6e69ac64afc5437feb8e40
AK CHAVAN <90214281+iamakchavan@users.noreply.github.com>
2025-04-06 17:13:52 +0530

    chore: update .gitignore and next.config.js for improved configuration
    - Expanded .gitignore to include additional cache, build, and editor files for better project cleanliness.
    - Updated next.config.js to use remotePatterns for image domains, enhancing image loading configuration.
    
    Co-Authored-By: PritiPriya <190529669+bytesizeddiva@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
